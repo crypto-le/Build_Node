99a25b2f69b8f0b55d65980fe8c10fedb2de64a9
790228724@qq.com <790228724@qq.com>
2022-05-07 18:41:44 +0800

    bsc 节点搭建教程

---
+++ Binance/start.sh
@@ -0,0 +1,3 @@
+nohup ./geth_linux --config ./config.toml --datadir ./node --diffsync --cache 75000 --rpc.allow-unprotected-txs --txlookuplimit 0 --ws --ws.port 29901 --ws.addr 0.0.0.0 --ws.origins "*" & 
+
+
