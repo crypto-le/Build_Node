30e4a36c060d810ae54b1b6d0c94949aa603d389
小财神 <790228724@qq.com>
2022-11-14 16:59:44 +0800

    Update start.sh
    添加BNB48 快照同步的启动脚本

--- Binance/start.sh
+++ Binance/start.sh
@@ -1,3 +1,7 @@
+
+#使用官方的快照来同步
 nohup ./geth_linux --config ./config.toml --datadir ./node --diffsync --cache 75000 --rpc.allow-unprotected-txs --txlookuplimit 0 --ws --ws.port 29901 --ws.addr 0.0.0.0 --ws.origins "*" & 
 
+#使用BNB48的快照来同步 需要制定同步模式 和 设置 pruneancient
+ ./geth_linux --config ./config.toml --datadir ./node --diffsync --cache 75000 --syncmode=full --tries-verify-mode=none --pruneancient=true  --rpc.allow-unprotected-txs --txlookuplimit 0 --ws --ws.port 29901 --ws.addr 0.0.0.0 --ws.origins "*"
 
