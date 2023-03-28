4baedc818b6d14d71710539e8cc4e522ae625ae5
小财神 <790228724@qq.com>
2023-03-28 15:33:34 +0800

    Update start.sh

--- Ethereum/start.sh
+++ Ethereum/start.sh
@@ -1,1 +1,3 @@
 nohup ./geth --config /eth/config.toml --datadir /eth/data --authrpc.jwtsecret /eth/prysm/jwt.hex --authrpc.port 29902  >> eth.out 2>&1 &
+#下面是优化版本启动
+nohup ./geth --config /eth/config.toml --cache 8192 --datadir /eth/data --authrpc.jwtsecret /eth/prysm/jwt.hex --authrpc.port 29902  >> eth.out 2>&1 &
